745df3f2f46740002c30a753b037f3d93344a8e4
haokngoc <haooila7@gmail.com>
2023-12-07 11:03:16 +0700

    update v.2

--- src/main.cpp
+++ src/main.cpp
@@ -61,52 +61,78 @@ enum {
 
 class MsgHandler {
 public:
-    MsgHandler(sockpp::tcp_socket& socket) : socket_(socket) {}
+    MsgHandler(sockpp::tcp_acceptor& acceptor) : acceptor_(acceptor) {}
 
-    int parse_Msg_Client() {
-        ssize_t n_read_bytes;
-        unsigned char buf[5];
-        memset(buf, 0, sizeof(buf));
+    void handleConnections() {
+        while (true) {
+            sockpp::inet_address peer;
+            sockpp::tcp_socket sock = acceptor_.accept(&peer);
+
+            if (!sock) {
+                cerr << "Error accepting incoming connection: " << acceptor_.last_error_str() << endl;
+                continue;
+            }
+
+            cout << "Received a connection request from " << peer << std::endl;
+
+            // Gọi đối tượng MsgHandler để xử lý gói tin từ client
+            int result = parse_Msg_Client(sock);
+            if (result == DET_STATE_SLEEP) {
+                cout << "Change working status: DET_STATE_SLEEP" << endl;
+            } else if (result == DET_STATE_WORK) {
+                cout << "Change working status: DET_STATE_WORK" << endl;
+            } else {
+                cout << "Error" << endl;
+            }
+
+            // Đóng kết nối với client
+            sock.close();
+            cout << "Connection closed from " << peer << endl;
+        }
+    }
+
+private:
+    sockpp::tcp_acceptor& acceptor_;
+    ssize_t n_read_bytes;
+    unsigned char buf[5];
+//    memset(buf, 0, sizeof(buf));
+    int parse_Msg_Client(sockpp::tcp_socket& socket) {
+
+
         int ret = -1;
-	try {
-		/*------------------- Exception case -------------------------*/
-		//1.Không đọc đủ số byte mong đợi: Khi client gửi ít hơn 5 byte mong đợi. Xử lý ngoại lệ này bằng cách thêm: throw std::runtime_error("Not enough bytes read");
-			/*VD: Client chỉ gửi 4 byte
-				HeadMarker = 0xAA;
-				Command = 0x58;
-				Param1 = 0x2b;
-				Param2 = 0x55; */
-		//2.Marker không khớp với giá trị mong đợi.
-		n_read_bytes = socket_.read(buf,size(buf));
-		if (n_read_bytes < sizeof(buf)) {
-			throw std::runtime_error("Not enough bytes read");
-		}
 
-		if (buf[0] != MARKER_HEAD || buf[4] != MARKER_TAIL) {
-			throw std::runtime_error("Invalid markers");
-		}
+        try {
+            // Đọc dữ liệu từ socket
+            n_read_bytes = socket.read(buf, sizeof(buf));
 
-		switch (buf[1]) {
-			case 0x1b:
-				ret = DET_STATE_SLEEP;
-				break;
-			case 0x58:
-				ret = DET_STATE_WORK;
-				break;
-			default:
-				ret = -1;
-				break;
-		}
+            // Kiểm tra xem đã đọc đúng số byte hay không
+            if (n_read_bytes < sizeof(buf)) {
+                throw std::runtime_error("Not enough bytes read");
+            }
 
-	} catch (const std::exception& e) {
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
+            // Kiểm tra marker không khớp
+            if (buf[0] != MARKER_HEAD || buf[4] != MARKER_TAIL) {
+                throw std::runtime_error("Invalid markers");
+            }
 
+            // Xử lý dữ liệu
+            switch (buf[1]) {
+                case 0x1b:
+                    ret = DET_STATE_SLEEP;
+                    break;
+                case 0x58:
+                    ret = DET_STATE_WORK;
+                    break;
+                default:
+                    ret = -1;
+                    break;
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+        }
 
         return ret;
     }
-private:
-    sockpp::tcp_socket& socket_;
 };
 
 int main(int argc, char* argv[]) {
@@ -121,33 +147,9 @@ int main(int argc, char* argv[]) {
 
     cout << "Awaiting connections on port " << port << "..." << endl;
 
-    while (true) {
-        sockpp::inet_address peer;
-        sockpp::tcp_socket sock = acc.accept(&peer);
-
-        if (!sock) {
-            cerr << "Error accepting incoming connection: " << acc.last_error_str() << endl;
-            continue;
-        }
-
-        cout << "Received a connection request from " << peer << std::endl;
-
-        // gọi đối tượng MsgHandler
-        MsgHandler msgHandler(sock);
-
-        // Nhận và xử lý gói tin từ client
-        int result = msgHandler.parse_Msg_Client();
-        if (result == DET_STATE_SLEEP) {
-            cout << "Change working status: DET_STATE_SLEEP" << endl;
-        } else if (result == DET_STATE_WORK) {
-            cout << "Change working status: DET_STATE_WORK" << endl;
-        } else {
-            cout << "Error" << endl;
-        }
-        // Đóng kết nối với client
-        sock.close();
-        cout << "Connection closed from " << peer << endl;
-    }
+    //đối tượng MsgHandler và xử lý kết nối
+    MsgHandler msgHandler(acc);
+    msgHandler.handleConnections();
 
     return 0;
 }
