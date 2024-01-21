b5d034304bcb42777ad54d1ad30faf147219b2ba
Duc-Long, Le <duclong.linux@gmail.com>
2024-01-21 08:24:11 -0500

    Add definition in common.h

--- src/Common.h
+++ src/Common.h
@@ -1,8 +1,12 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-//#define BUFFER_SIZE (2*3456*3180)
-#define BUFFER_SIZE 21*1024*1024
+
+#define N_BUF 3
+#define PRB_WIDTH_DEFAULT			3456
+#define PRB_HEIGHT_DEFAULT			3180
+#define TRIGGER_REG	0x0a
+#define BUFFER_SIZE (2*PRB_WIDTH_DEFAULT*PRB_HEIGHT_DEFAULT)
 #define BUFFER_CAL_MD5 50*1024
 #define MARKER_HEAD 0xAA
 #define MARKER_TAIL 0x55
@@ -10,16 +14,7 @@
 #define DET_STATE_SLEEP 0x1b
 #define DET_STATE_CLOSE 0x2b
 #define DET_STATE_TRIGGER 0x66
-#define CHUNK_SIZE 1024*10
+#define CHUNK_SIZE 1024*30
 #define TIMEOUT_MICRO_SECONDS 100000000000
 
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/async.h"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-
-
-
 #endif
-
